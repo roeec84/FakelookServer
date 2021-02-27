c1854007b0be5e9bd2753429fea96ac8e3355671
Roee Cohen <roeec84@gmail.com>
2021-02-27 10:39:40 +0200

    Added repositories

--- models/comment.js
+++ models/comment.js
@@ -13,6 +13,12 @@ const commentSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
+    text: {
+        type: String,
+        minlength: 1,
+        maxlength: 100,
+        required: true,
+    },
     tags: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Tag',
@@ -25,7 +31,8 @@ const validate = (comment) =>{
     const schema = Joi.object({
         post: Joi.objectId().required().label('Post'),
         user: Joi.objectId().required().label('User'),
-        tags: Joi.any()
+        text: Joi.string().min(1).max(100).required().label('Text'),
+        tags: Joi.array().items(Joi.objectId()).label('Tags')
     })
     return schema.validate(comment, {
         abortEarly: false
---
+++ repositories/tagRepository.js
@@ -0,0 +1,37 @@
+const { Tag } = require('../models/tag');
+
+const addTag = async (title) => {
+    const newTag = new Tag({
+        title: title
+    })
+    try {
+        const tag = await newTag.save();
+        if(tag)
+            return tag
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+}
+
+const getAllTags = async () => {
+    try {
+        const tags = await Tag.find();
+        return tags;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getTagByTitle = async (title) => {
+    try {
+        const tag = await Tag.findOne({title: title});
+        return tag;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports.addTag = addTag;
+module.exports.getAllTags = getAllTags;
+module.exports.getTagByTitle = getTagByTitle;
